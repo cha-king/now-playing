9ee30a66eea4f891ecd345c9ce980bda251525a1
Charlie King <charlie.king@verkada.com>
2021-12-07 21:45:33 -0800

    Add now-playing styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import RecentlyPlayed from './RecentlyPlayed';
 import NowPlaying from './NowPlaying';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div>
+    <div className={styles.app}>
       <NowPlaying/>
       {/* <RecentlyPlayed/> */}
     </div>
--- frontend/src/NowPlaying.js
+++ frontend/src/NowPlaying.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-import Song from './Song.js';
+import styles from './NowPlaying.module.css';
 
 function NowPlaying() {
     const [song, setSong] = useState(null);
@@ -18,10 +18,20 @@ function NowPlaying() {
     }, []);
 
     if (song === null) {
-        return <div>Nothing is currently playing</div>
+        return null
     }
     
-    return <Song title={song.name} artist={song.artist} album={song.album} imageUrl={song.image_url}/>
+    // return (<Song title={song.name} artist={song.artist} album={song.album} imageUrl={song.image_url}/>)
+    return (
+        <div className={styles['now-playing']}>
+            <img src={song.image_url} alt={song.album}/>
+            <ul>
+                <li className={styles['song-name']}>{song.name}</li>
+                <li className={styles['album-name']}>{song.album}</li>
+                <li className={styles['artist-name']}>{song.artist}</li>
+            </ul>
+        </div>
+    )
 }
 
 export default NowPlaying;
