8dd0abcbc1c03fb50557e50327dcc2e55410794e
Charlie King <charlie.king@verkada.com>
2021-12-11 16:11:15 -0800

    Push song changes to websockets

--- backend/now-playing/spotify.py
+++ backend/now-playing/spotify.py
@@ -1,15 +1,22 @@
 import asyncio
 import datetime
+import logging
 import time
-from asyncio import Task
+from asyncio import Task, Future
 from typing import Optional
 
+from fastapi import WebSocket
 from httpx import AsyncClient
+
+from .schema import Song
 
 
 TOKEN_URL = "https://accounts.spotify.com/api/token"
 
 POLL_TIME = datetime.timedelta(seconds=1)
+
+
+logger = logging.getLogger(__name__)
 
 
 class Spotify:
@@ -18,6 +25,7 @@ class Spotify:
         self._token = AccessToken(self._client, client_id, client_secret, refresh_token)
         self.currently_playing: Optional[dict] = None
         self._task: Task = Optional[None]
+        self._websockets: set[WebSocket] = set()
 
     async def get_recently_played(self, limit: int = 5) -> dict:
         access_token = await self._token.get()
@@ -47,6 +55,7 @@ class Spotify:
     def start(self):
         self._task = asyncio.create_task(self._loop())
 
+        # TODO: Figure out how to kill main event loop when exception is raised
         def handle_exception(future: Future):
             if future.cancelled():
                 return
@@ -57,11 +66,50 @@ class Spotify:
 
     def stop(self):
         self._task.cancel()
+        logger.info("get_currently_playing task canceled")
+
+    def subscribe(self, websocket: WebSocket):
+        self._websockets.add(websocket)
 
     async def _loop(self):
+        logger.info("Starting get_currently_playing task..")
         while True:
-            self.currently_playing = await self.get_currently_playing()
             await asyncio.sleep(POLL_TIME.total_seconds())
+            currently_playing = await self.get_currently_playing()
+            logger.debug("Acquired song")
+
+            if currently_playing:
+                new_id = currently_playing["item"]["id"]
+            else:
+                new_id = None
+
+            if self.currently_playing:
+                current_id = self.currently_playing["item"]["id"]
+            else:
+                current_id = None
+
+            if new_id == current_id:
+                continue
+
+            logger.debug("Song has changed")
+
+            if currently_playing:
+                song = Song(
+                    name=currently_playing['item']['name'],
+                    artist=currently_playing['item']['artists'][0]['name'],
+                    album=currently_playing['item']['album']['name'],
+                    href=currently_playing['context']['external_urls']['spotify'],
+                    image_url=currently_playing['item']['album']['images'][0]['url']
+                )
+                song_json = dict(song)
+            else:
+                song_json = {}
+
+            aws = [websocket.send_json(song_json) for websocket in self._websockets]
+            await asyncio.gather(*aws)
+            logger.debug("Published song to websockets")
+
+            self.currently_playing = currently_playing
 
 
 class AccessToken:
@@ -90,3 +138,4 @@ class AccessToken:
 
         self._access_token = response_json['access_token']
         self._expires_at = response_json['expires_in'] + time.time() - 30
+        logger.debug("Acquired access token")
