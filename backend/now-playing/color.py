a280225f435d83f2d3d110828a04014112c19b2e
Charlie King <charlie.king@verkada.com>
2021-12-28 18:07:04 -0700

    Add color module

---
+++ backend/now-playing/color.py
@@ -0,0 +1,34 @@
+import httpx
+from PIL import Image
+
+from io import BytesIO
+from typing import List, Tuple
+
+
+QUANTIZATION_FACTOR = 64
+
+
+def get_colors_from_url(client: httpx.Client, url: str) -> List[Tuple]:
+    img = load_image(client, url)
+    colors = get_colors(img)
+    return colors
+
+
+def load_image(client: httpx.Client, url: str) -> Image:
+    response = client.get(url)
+    response.raise_for_status()
+
+    img = Image.open(BytesIO(response.content))
+
+    return img
+
+
+def get_colors(img: Image, n=4) -> List[Tuple]:
+    q_img = img.quantize(QUANTIZATION_FACTOR)
+
+    pallete = q_img.getpalette()
+    colors = [tuple(pallete[i:i+3]) for i in range(0, 64*3, 3)]
+
+    dom_colors_p = sorted(q_img.getcolors(), key=lambda x: x[0], reverse=True)[:n]
+    dom_colors = list(map(lambda x: colors[x[1]], dom_colors_p))
+    return dom_colors
